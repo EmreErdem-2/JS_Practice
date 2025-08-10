0630f5eb21ef4fe86842f1ec9cb3d8928edcca7c
Emre Erdem <emreerdem95@outlook.com>
2025-08-10 20:54:03 +0300

    created style file and started working on button elements

--- Calculator/calc_script.js
+++ Calculator/calc_script.js
@@ -1,1 +1,38 @@
 
+
+
+function operator(a,operator,b){
+    let result = 0;
+    switch (operator) {
+    case '=':
+        //print result to diplay
+        break;
+    case '+':
+        result = add(a,b);
+        break;
+    case '-':
+        result = subtract(a,b);
+        break;
+    case '*':
+        result = multiply(a,b);
+        break;
+    case '/':
+        result = divide(a,b);
+        break;
+    default:
+    
+    }
+}
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return Math.round((a/b) * 100) / 100;
+}
